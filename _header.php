fbfc23033d614205544b2c44669b654c4b41420a
Guilherme Gregorio <149717965+Guilherme0308@users.noreply.github.com>
2024-10-14 20:25:56 -0300

    Add files via upload

--- _header.php
+++ _header.php
@@ -2,31 +2,33 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="/assets/css/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $site_name ?><?php echo $page_title ?></title>
+    <title><?php $site_name ?></title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
 </head>
 <body>
     <header>
-            <a href="/" title="Página inicial"><img src="<?php echo $site_logo ?>" >
-        <div class="search-container">
-            <input type="text" placeholder="Pesquisar">
-            <button type="submit"><i class="fa fa-search"></i></button>
-        </div>
+        <img src="/src/img/logo_pizzaria.png" alt="Logo da Pizzaria" class="logo">
+   
         <ul class="nav">
+
             <li><a href="/index.html">Home</a></li>
             <li><a href="/cardapio.html">Cardápio</a></li>
             <li><a href="/fazer_pedido.html">Fazer Pedido</a></li>
-            <li><a href="#">Contatos</a></li>
-            <li><a href="#">Sobre</a></li>
-            <li><a href="/login.html" class="login">login</a></li>
-            <li><a href="#" class="profile"><i class="fa fa-user"></i></a></li>
-            <li><a href="#" class="cart"><i class="fa fa-shopping-cart"></i></a></li>
+            <li><a href="/contatos.html">Contatos</a></li>
+            <li><a href="/sobre.html">Sobre</a></li>
+            <li><a href="/login.html" class="login">Login</a></li>
+            <li><a href="/profile.html" class="profile"><i class="fa fa-user"></i></a></li>
+            <li class="cart">
+                <a href="/carrinho.html"><i class="fa fa-shopping-cart cart-icon"></i>
+                <span class="cart-count"></span></a>
+            </li>
+            <div class="search-container">
+                <input type="text" placeholder="Pesquisar...">
+                <button type="submit"><i class="fa fa-search"></i></button>
+            </div>
         </ul>
-
+        <button class="menu-toggle"><i class="fa fa-bars"></i></button>
     </header>
-    <section class="home-banner">
-        <img src="/src/img/pizza-5107039_640.jpg" alt="Delicious pizza" class="banner">
-        <img src="/src/img/logo_pizzaria.png" alt="Pizzeria logo" class="logo-banner">
-        <button type="submit" class="banner-button">Fazer Pedido</button>
-    </section>
